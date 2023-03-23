d8c3a7e9fdd878c8c96d4fe23b4ec58c0d044503
HerMak56 <gdevcic10@gmail.com>
2023-03-23 15:09:17 +0300

    Add some code

--- MotorLib/MotorTest.ino
+++ MotorLib/MotorTest.ino
@@ -5,15 +5,15 @@ Motor A;
 #define in2 6
 #define Encoder1 2
 #define Encoder2 3
-void Flag()
-{
-    A.FlagInterrapt();
-}
 void setup()
 {
     Serial.begin(9600);
     A.init(Encoder1,Encoder2,PWM, in1,in2);
     attachInterrupt(digitalPinToInterrupt(Encoder1),Flag,CHANGE);
+}
+void Flag()
+{
+    A.Flag();
 }
 void loop()
 {
